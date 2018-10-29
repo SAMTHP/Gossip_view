3aad09839f5bc3ea6ad940a145991d096687b8e8
SAMTHP <samir_615@live.fr>
2018-10-29 19:14:57 +0100

    These gossips are very bad git add .

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,35 @@
+class StaticController < ApplicationController
+
+  def index
+  	@all_gossips = Gossip.all
+  end
+
+  def index_post
+  	@user = User.new
+  	@user.first_name = params["first_name"]
+  	p @user.first_name
+  	@user.last_name = params["last_name"]
+  	p @user.last_name
+  	p @user
+  	@user.save
+  	redirect_to  "/welcome/#{@user.first_name}"
+  end
+
+  def team
+  end
+
+  def contact
+  end
+
+  def welcome
+  	@first_name =  params["first_name"]
+  end
+
+  def gossip
+  	puts "Regarde ici"
+  	@gossip_author = params["author"]
+  	p @gossip_author
+  	@user_gossip = Gossip.all.find_by(author:"Radagast")
+  	p @user_gossip
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+10.times do
+	User.create(first_name: Faker::Hobbit.character, last_name: Faker::Hobbit.character)
+end
+
+1000.times do
+	Gossip.create(author: Faker::Hobbit.character, content: Faker::HowIMetYourMother.quote, user_id: rand(1..10))
+end
